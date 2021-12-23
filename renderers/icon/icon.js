72bfe93d34aedf96b336ade0cf97d5a286937f76
Jose Alemán <mail@mandelrot.com>
2021-12-23 15:14:12 +0000

    Notifications handling added

--- renderers/icon/icon.js
+++ renderers/icon/icon.js
@@ -4,12 +4,14 @@ const {ipcRenderer} = require('electron');
 const iconBasicPath = '../../resources/static/main-icons/icon-basic.png';
 const iconNotifyPath = '../../resources/static/main-icons/icon-notify.png';
 
-
 const iconImage = document.getElementById('iconImage');
 
 window.onload = function () {
   iconImage.setAttribute('style', 'background-image: url(' + iconBasicPath + ')');
 }
+
+let notifications = 0;
+
 
 
 /* Making the icon draggable */
@@ -18,9 +20,12 @@ let mouseX;
 let mouseY;
 
 function onMouseDown(e) {
+  // Cleaning notifications if any
+  notifications = 0;
+  updateLookUponNotifications();
+  // Dragging
   mouseX = e.clientX;  
   mouseY = e.clientY;
-  
   document.addEventListener('mouseup', onMouseUp)
   requestAnimationFrame(this.moveWindow);
 }
@@ -43,18 +48,28 @@ function onContextmenu() {
 }
 
 
+/* Incoming notifications */
+ipcRenderer.on('notification', () => {
+  notifications ++;
+  updateLookUponNotifications();
+});
+
+function updateLookUponNotifications() {
+  if (notifications === 0) {
+    iconImage.innerHTML = '';
+    iconImage.setAttribute('style', 'background-image: url(' + iconBasicPath + ')');
+  } else {
+    iconImage.innerHTML = `
+      <span>${notifications}</span>
+    `;
+    iconImage.setAttribute('style', 'background-image: url(' + iconNotifyPath + ')');
+  }
+}
+
+
+
 /* 
-To be included by the final developer: 
-
-  - Communication with the main process
-
-  - When receiving a message saying there are updates to check:
-  switch between logos (interval) creating a "blinking" effect,
-  to tell the user there's some activity that needs attention
-
-Optional: 
-
-  - Include css add-ons like animated badges, icons related
+  Optional: include css add-ons like animated badges, icons related
   to the upcoming notification, etc. The .html file makes 
   further integrations very easy.
-*/
+*/ 
